70aa8ccdf03c7546c596d1fe6656f1a15f531db6
michal <shugo12345@gmail.com>
2019-07-25 16:26:33 +0200

    Add more details to transaction table

--- src/components/TransactionTable/TransactionTable.js
+++ src/components/TransactionTable/TransactionTable.js
@@ -10,12 +10,19 @@ class TransactionTable extends React.Component {
 
     this.state = {
       value: '',
+      toggledDetails: null,
     }
+  }
+
+  toggleDetails = (id) => {
+    this.setState({
+      toggledDetails: this.state.toggledDetails === id ? null : id,
+    })
   }
 
   render() {
     return (
-      <Table responsive="lg">
+      <Table responsive="lg" className="TransactionTable">
         <thead>
           <tr>
             <th>Hash</th>
@@ -24,19 +31,42 @@ class TransactionTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          {this.props.transactions.map(transaction =>{
+          {this.props.transactions.map((transaction, index) =>{
             const isSendingMoney = transaction.inputs.find(t => t.prev_out.addr === this.props.address);
-            
-            return (<tr className={isSendingMoney ? 'row-send' : 'row-recieve'}>
-              <td>{transaction.hash}</td>
-              <td>{transaction.date}</td>
-              <td>{isSendingMoney ?
-                `- ${satoshiBitcoin.toBitcoin(transaction.inputs
-                    .filter(t => t.prev_out.addr === this.props.address)
-                    .reduce((prev, current) => prev + current.prev_out.value, 0))}` :
-                `+ ${satoshiBitcoin.toBitcoin(transaction.out.filter(t => t.addr === this.props.address)
-                  .reduce((prev, current) => prev + current.value, 0))}`}</td>
-            </tr>)
+            const activeToggle = this.state.toggledDetails === index+1;
+            return (
+              <>
+                <tr key={`row-${index}`} className={`${isSendingMoney ? 'row-send' : 'row-recieve'}
+                  ${activeToggle ? 'row-active' : null}`}
+                  onClick={() => { this.toggleDetails(index+1) }}>
+                  <td>{transaction.hash}</td>
+                  <td>{new Date(transaction.time*1000).toLocaleString()}</td>
+                  <td>{isSendingMoney ?
+                    `- ${satoshiBitcoin.toBitcoin(transaction.inputs
+                        .filter(t => t.prev_out.addr === this.props.address)
+                        .reduce((prev, current) => prev + current.prev_out.value, 0))}` :
+                    `+ ${satoshiBitcoin.toBitcoin(transaction.out.filter(t => t.addr === this.props.address)
+                      .reduce((prev, current) => prev + current.value, 0))}`}</td>
+                </tr>
+                {activeToggle ?
+                  <tr className="TransactionTable-Details">
+                    <td>
+                      <h5>From</h5>
+                      {transaction.inputs.map(acc => <p>{acc.prev_out.addr}</p>)}
+                    </td>
+                    <td>
+                      <div
+                        className="arrow-right"
+                        style={{ borderLeftColor: isSendingMoney ? 'red' : 'green' }}/>
+                    </td>
+                    <td>
+                      <h5>To</h5>
+                      {transaction.out.map(acc => <p>{acc.addr}</p>)}
+                    </td>
+
+                  </tr> :
+                  null}
+              </>)
           })}
         </tbody>
       </Table>
